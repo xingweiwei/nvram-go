8bf07ecc10fde71584596e326e593699730bcffe
weiwei.xing <weiwei.xing@soulinfo.com>
2013-10-02 22:21:46 +0800

    support nvram show/get

--- nvram.go
+++ nvram.go
@@ -5,12 +5,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	//"io"
+	"io"
 	"log"
 	"os"
-	//"strings"
+	"strings"
 	"sync"
 )
 
@@ -19,48 +20,110 @@ const (
 )
 
 type nvram struct {
-	lock *sync.RWMutex
-	path string
-	data map[string]map[string]string
+	lock    *sync.RWMutex
+	path    string
+	keyList []string
+	data    map[string]string
 }
 
 func newNvram() *nvram {
 	n := new(nvram)
 	n.lock = new(sync.RWMutex)
-	n.data = make(map[string]map[string]string)
+	n.data = make(map[string]string)
 	return n
 }
 
 func loadFile(path string) (*nvram, bool) {
+	var f *os.File
+	var err error
+	if f, err = os.Open(path); err != nil {
+		return nil, false
+	}
+
+	defer f.Close()
+
 	n := newNvram()
+	// Create buffer reader.
+	buf := bufio.NewReader(f)
+
+	for {
+		line, err := buf.ReadString('\n')
+		line = strings.TrimSpace(line)
+
+		if err != nil {
+			// Unexpected error
+			if err != io.EOF {
+				return nil, false
+			}
+
+			// Reached end of file, if nothing to read then break,
+			// otherwise handle the last line.
+			if len(line) == 0 {
+				break
+			}
+		}
+
+		// switch written for readability (not performance)
+		switch {
+		case len(line) == 0: // Empty line
+			continue
+		case line[0] == '#' || line[0] == ';': // Comment
+			continue
+
+		default: // Other alternatives
+			i := strings.IndexAny(line, "=:")
+			if i > 0 {
+				key := strings.TrimSpace(line[0:i])
+				value := strings.TrimSpace(line[i+1:])
+				n.Set(key, value)
+
+			}
+		}
+
+		// Reached end of file
+		if err == io.EOF {
+			break
+		}
+	}
+
 	return n, true
 }
 
 func saveFile(n *nvram) bool {
-	fmt.Println(os.Stderr, "Save file!")
+	//fmt.Println(os.Stderr, "Save file!")
 	os.Exit(2)
 	return false
 }
 
 func (this *nvram) Show() string {
-	for _, n := range this.data {
-		fmt.Println(os.Stderr, n)
+	for _, k := range this.keyList {
+		value := this.data[k]
+		fmt.Printf("%s=%s\n", k, value)
 	}
 	return "ERROR"
 }
 
 func (this *nvram) Get(key string) string {
 	if _, ok := this.data[key]; ok {
-		return this.data[key][key]
+		return this.data[key]
 	}
-	return "ERROR"
+
+	return fmt.Sprintf("key[%s] is not exist", key)
 }
 
 func (this *nvram) Set(key, value string) bool {
-	fmt.Println(os.Stderr, "key=", key, "value=", value)
-	n := make(map[string]string)
-	n[key] = value
-	this.data[key] = n
+	//fmt.Printf("%s=%s", key, value)
+	var isExist bool = false
+	this.data[key] = value
+	for _, k := range this.keyList {
+		if k == key {
+			isExist = true
+			break
+		}
+	}
+	if isExist == false {
+		this.keyList = append(this.keyList, key)
+	}
 	return true
 }
 
@@ -100,7 +163,8 @@ func main() {
 		if len(args) < 2 {
 			return
 		}
-		nvram.Get(args[1])
+		value := nvram.Get(args[1])
+		fmt.Printf("%s\n", value)
 	case "show":
 		nvram.Show()
 	default:
